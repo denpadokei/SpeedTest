64e697c100e825c5b6abc678eef2a477ae19a01d
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-09-25 18:52:11 +0900

    internal commit

---
+++ Assets/Scripts/SpeedTestController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedTestController : MonoBehaviour
+{
+    // 100万回
+    public const int COUNT = 1000000;
+    
+    private ObjectMemoryPool<Component> _objectMemory1;
+    private ObjectMemoryPool<TestAccessClass> _objectMemory2;
+    private static string _message;
+    private void Awake()
+    {
+        this._objectMemory1 = new ObjectMemoryPool<Component>(5, () => new GameObject("Test", typeof(TestAccessClass)).GetComponent<TestAccessClass>());
+        this._objectMemory2 = new ObjectMemoryPool<TestAccessClass>(5, () => new GameObject("Test", typeof(TestAccessClass)).GetComponent<TestAccessClass>());
+    }
+
+    private void Start()
+    {
+        var btton = this.gameObject.GetComponent<Button>();
+        btton.onClick.AddListener(this.SpeedTest);
+    }
+
+    private void SpeedTest()
+    {
+        UnityEngine.Debug.Log("Start");
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        for (int i = 0; i < COUNT; i++) {
+            var item = this._objectMemory1.Alloc();
+            item.gameObject.SetActive(true);
+            _message = item.GetType().GetField("_guid", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(item).ToString();
+            item.gameObject.SetActive(false);
+            this._objectMemory1.Free(item);
+        }
+        stopwatch.Stop();
+        UnityEngine.Debug.Log($"毎回GetType、GetFieldしてGetValue:{stopwatch.ElapsedMilliseconds}ms");
+        stopwatch.Reset();
+
+        var typeObject = this._objectMemory1.Alloc();
+        var fieldInfo = typeObject.GetType().GetField("_guid", BindingFlags.NonPublic | BindingFlags.Instance);
+        this._objectMemory1.Free(typeObject);
+        stopwatch.Start();
+        for (int i = 0; i < COUNT; i++) {
+            var item = this._objectMemory1.Alloc();
+            item.gameObject.SetActive(true);
+            _message = fieldInfo.GetValue(item).ToString();
+            item.gameObject.SetActive(false);
+            this._objectMemory1.Free(item);
+        }
+        stopwatch.Stop();
+        UnityEngine.Debug.Log($"FieldInfo固定でGetValue:{stopwatch.ElapsedMilliseconds}ms");
+        stopwatch.Reset();
+
+        stopwatch.Start();
+        for (int i = 0; i < COUNT; i++) {
+            var item = this._objectMemory2.Alloc();
+            item.gameObject.SetActive(true);
+            _message = item.GUID;
+            item.gameObject.SetActive(false);
+            this._objectMemory2.Free(item);
+        }
+        stopwatch.Stop();
+        UnityEngine.Debug.Log($"プロパティの素アクセス:{stopwatch.ElapsedMilliseconds}ms");
+        stopwatch.Reset();
+
+        stopwatch.Start();
+        for (int i = 0; i < COUNT; i++) {
+            var item = this._objectMemory2.Alloc();
+            item.gameObject.SetActive(true);
+            _message = item.g_guid;
+            item.gameObject.SetActive(false);
+            this._objectMemory2.Free(item);
+        }
+        stopwatch.Stop();
+        UnityEngine.Debug.Log($"メンバ変数への素アクセス:{stopwatch.ElapsedMilliseconds}ms");
+        stopwatch.Reset();
+
+        UnityEngine.Debug.Log($"Finish");
+    }
+}
---
+++ Assets/Scripts/TestAccessClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAccessClass : MonoBehaviour
+{
+    private string _guid;
+    public string GUID { get => _guid; set => _guid = value; }
+    public string g_guid;
+
+    private void Awake()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        this.SetGUID();
+    }
+
+    public void SetGUID()
+    {
+        this._guid = Guid.NewGuid().ToString();
+        this.g_guid = this._guid;
+    }
+}
